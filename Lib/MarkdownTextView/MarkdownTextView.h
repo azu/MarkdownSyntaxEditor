8f28f0ec08f18b5881fd1b000d56066bf9de1f4f
azu <azuciao@gmail.com>
2013-10-26 19:21:27 +0900

    add MarkdownTextView that apply MarkdownSyntax.

---
+++ Lib/MarkdownTextView/MarkdownTextView.h
@@ -0,0 +1,12 @@
+//
+// Created by azu on 2013/10/26.
+//
+
+
+#import <Foundation/Foundation.h>
+
+@class MarkdownSyntaxGenerator;
+
+
+@interface MarkdownTextView : UITextView <UITextViewDelegate>
+@end
--- MarkdownSyntaxEditor/EditorViewController.m
+++ MarkdownSyntaxEditor/EditorViewController.m
@@ -4,38 +4,19 @@
 
 
 #import "EditorViewController.h"
-#import "MarkdownSyntaxGenerator.h"
+#import "MarkdownTextView.h"
 
 
 @implementation EditorViewController {
 
-    __weak IBOutlet UITextView *editorTextView;
+    __weak IBOutlet MarkdownTextView *editorTextView;
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self updateSyntaxTextView:editorTextView];
+    editorTextView.contentInset = UIEdgeInsetsMake(0, 0,
+        0, 0);
 }
 
-- (void)updateSyntaxTextView:(UITextView *) textView {
-    MarkdownSyntaxGenerator *markdownSyntaxGenerator = [[MarkdownSyntaxGenerator alloc] init];
-    textView.scrollEnabled = NO;
-    NSRange selectedRange = textView.selectedRange;
-    NSArray *models = [markdownSyntaxGenerator syntaxModelsForText:textView.text];
-    NSMutableAttributedString *attributedString = [[NSMutableAttributedString alloc] initWithString:textView.text];
-    for (MarkdownSyntaxModel *model in models) {
-        [attributedString addAttributes:AttributesFromMarkdownSyntaxType(model.type) range:model.range];
-    }
-    textView.attributedText = attributedString;
-    textView.selectedRange = selectedRange;
-    textView.scrollEnabled = YES;
-}
-
-- (void)textViewDidChange:(UITextView *) textView {
-    if (![textView isEqual:editorTextView]) {
-        return;
-    }
-    [self updateSyntaxTextView:textView];
-}
 
 @end
